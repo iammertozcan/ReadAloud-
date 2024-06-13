5466927890d38001ef65f07a904e0b1257f6d31d
Mert Özcan <iammertozcan@gmail.com>
2024-06-13 11:50:44 +0300

    Create initial project structure

---
+++ ReadAloudApp/ReadAloudApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ReadAloudApp
+//
+//  Created by Mert Özcan on 13.06.2024.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
